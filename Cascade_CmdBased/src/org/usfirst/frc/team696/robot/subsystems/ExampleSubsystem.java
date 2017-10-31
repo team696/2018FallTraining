2cf8d91544c800961a15600a656d24c460a70b9d
clonetron <jgonza1427@gmail.com>
2017-10-31 14:55:20 -0700

    Started object mapping in susbystem.

--- Cascade_CmdBased/src/org/usfirst/frc/team696/robot/subsystems/ExampleSubsystem.java
+++ Cascade_CmdBased/src/org/usfirst/frc/team696/robot/subsystems/ExampleSubsystem.java
@@ -1,13 +1,20 @@
 package org.usfirst.frc.team696.robot.subsystems;
 
+import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class ExampleSubsystem extends Subsystem {
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
+	
+	TalonSRX mastershootertalon = new TalonSRX(1);
+	VictorSP gearintake = new VictorSP(11);
+	TalonSRX gearpivot = new TalonSRX(13);
+	
+	
+	
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
