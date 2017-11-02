21f00a81b0257e2e3092d9618f45180e0152175f
Moonsmithy <rerk9201@gmail.com>
2017-11-02 14:23:16 -0700

    Added code for Example Subsytem

--- Cascade_CmdBased/src/org/usfirst/frc/team696/robot/subsystems/ExampleSubsystem.java
+++ Cascade_CmdBased/src/org/usfirst/frc/team696/robot/subsystems/ExampleSubsystem.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team696.robot.subsystems;
 
-import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -8,14 +7,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class ExampleSubsystem extends Subsystem {
-	
-	TalonSRX mastershootertalon = new TalonSRX(1);
-	VictorSP gearintake = new VictorSP(11);
-	TalonSRX gearpivot = new TalonSRX(13);
-	
-	
-	
+	VictorSP sideswipemotor;
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 
+	public ExampleSubsystem(int sideswipeport) {
+		this.sideswipemotor = new VictorSP(sideswipeport);
+		
+	}
+	
+	
+	
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
